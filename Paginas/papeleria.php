6f0a385346d47e1c8da7eaf0648d88e3707f0af9
GerardGRR <gegaramirezro@ittepic.edu.mx>
2024-07-26 11:37:16 -0600

    primer cambio

---
+++ Paginas/papeleria.php
@@ -0,0 +1,15 @@
+<?php
+    include('includes/encabezado.php');
+?>
+ 
+    <div class='producto-tarjeta'>
+            <h2 class='nombre'>Papel cascaron</h2>
+            <h3 class='precio'>2</h3>
+            <img src='../Imagenes/casca.jpeg' alt='' class='imagen'>
+            <p class='descripcion'>tamaño 20cm</p>
+            <button class='boton max'>Agregar al Carrito</button>
+    </div>;
+
+    <?php
+    include('includes/pie.php');
+?>
