5235efa93072ec7d93c3e29573e19ccca4eac453
GerardGRR <gegaramirezro@ittepic.edu.mx>
2024-07-27 00:42:26 -0600

    ac

--- Paginas/papeleria.php
+++ Paginas/papeleria.php
@@ -1,30 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Sitio para venta papeleria principito">
-    <meta name="keywords" content="utiles, libretas, papeleria, fiesta">
-    <title>Principito</title>
-    <link rel="stylesheet" href="../CSS/index.css">
-</head>
-
-<body>
-    <header class="componente">
-        <div class="logo">
-            <img src="../Imagenes/logo.png" alt="logo">
-        </div>
-        <div class="titulo">
-            <h1>PAPELERIA PRINCIPITO</h1>
-        </div> 
-        <div class="carrito">
-            <img src="../Imagenes/carrito.png" alt="carrito">
-        </div>
-    </header>
-
-
-
-    <main>
+<?php
+    include('items/encabezado.php');
+?>
         
         <div class='contenedor'>
             <div class="elemento">
@@ -67,22 +43,7 @@
                 <button class="boton">Agregar al carrito</button>
             </div>
         </div>
-</main>
-
-<footer class="componentef">
-    <div class="elementof">
-        <a class="contacto">CONTACTO</a>
-    </div>
-
-</footer>
-
-
-
-<div class="saturno">
-    <img src="../Imagenes/saturno.png" alt="saturno">
-</div>
-<div class="luna">
-        <img src="../Imagenes/luna.png" alt="luna">
-</div>
-</body>
-</html>
+        
+<?php
+    include('items/pie.php');
+?>
